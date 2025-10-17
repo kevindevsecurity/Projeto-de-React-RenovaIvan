35e898d177c740a7af865d7a215bc3956ec456c2
Kevin <kevin.ferreira0510@gmail.com>
2025-10-17 01:25:10 -0300

    Atualização Final React

--- src/componentes/Main.jsx
+++ src/componentes/Main.jsx
@@ -1,5 +1,5 @@
-import Contato from "./contato";
-import Depoimentos from "./depoimentos";
+import Contato from "./Contato";
+import Depoimentos from "./Depoimentos";
 import Inicio from "./Inicio";
 import Portfolio from "./portfolio";
 import Servicos from "./Servicos";
